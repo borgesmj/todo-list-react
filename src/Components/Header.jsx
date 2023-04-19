8246fe080b7e35fc93a3b3be79dae2d7daf2181b
Miguel José <121818423+borgesmj@users.noreply.github.com>
2023-04-19 13:55:49 -0500

    realizando una prueba de deploy automatico desde giyhub a netlify

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='container mx-auto h-16 text-center mt-5 p-5'>
-        <h1 className='font-black text-3xl'>To Do list</h1>
+        <h1 className='font-black text-3xl'>To-Do list</h1>
         <h2>Proyecto de práctica, utilizando React JavaScript, para mi <a href="https://borgesmj.github.io/" target='_blank' className='text-indigo-600 font-bold'>portfolio web</a></h2>
     </div> 
   )
